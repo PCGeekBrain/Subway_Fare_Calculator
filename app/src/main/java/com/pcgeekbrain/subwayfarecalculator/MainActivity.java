04e5803ce5b413e2fffcb7f009de136a7ed07eff
PCGeek <mendelh1537@gmail.com>
2016-08-14 15:56:59 -0400

    Fixed rounding up trips error

--- app/src/main/java/com/pcgeekbrain/subwayfarecalculator/MainActivity.java
+++ app/src/main/java/com/pcgeekbrain/subwayfarecalculator/MainActivity.java
@@ -6,17 +6,20 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText existingFunds, numOfTrips, totalToAdd;
+    private TextView amountRemaining;
     public static final String TAG = "MainActivity";
     //calculation variables
     private double fare = 2.75;
     private double rate = 1.11;
     private double min = 5.50;
+    double remaining_funds = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
         existingFunds = (EditText)findViewById(R.id.existing_funds);
         numOfTrips = (EditText)findViewById(R.id.num_of_trips);
         totalToAdd = (EditText)findViewById(R.id.total_to_add);
+        amountRemaining = (TextView)findViewById(R.id.amount_remaining);
+        amountRemaining.setText(getString(R.string.amount_left_test, remaining_funds));
 
         existingFunds.addTextChangedListener(ExistingFundsWatcher);
         numOfTrips.addTextChangedListener(numOfTripsWatcher);
@@ -37,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
         double existing = getDoubleFromEditText(existingFunds);
         double trips = getDoubleFromEditText(numOfTrips);
         double total = getDoubleFromEditText(totalToAdd);
+        int total_trips;
 
         switch (id){
             case R.id.existing_funds:
@@ -50,11 +56,22 @@ public class MainActivity extends AppCompatActivity {
                 setWatchedEditText(totalToAdd, totalToAddWatcher, String.format(Locale.US, "%.2f", total));
                 break;
             case R.id.total_to_add:
+                //add bonus if over min
                 if (total > min){
                     total = total * rate;
                 }
-                trips = (total + existing) / fare;
-                setWatchedEditText(numOfTrips, numOfTripsWatcher, String.format(Locale.US, "%.0f", trips));
+                trips = (total + existing) / fare;              //trips available is the amount of times you have the fare on the card
+                total_trips = (int)trips;                       //round that down to avoid rounding up errors (e.g. $5 = 2 trips)
+                remaining_funds = (trips - (int)trips) * fare;  //get whatever is left (remainder of trips times fare. Since trips is the total divided by fare this should work.
+
+                //debug
+                Log.d(TAG, "runCalculation: trips = " + trips);
+                Log.d(TAG, "runCalculation: total_trips = " + total_trips);
+                Log.d(TAG, "runCalculation: remaining_funds = " + remaining_funds);
+                //set text fields
+                setWatchedEditText(numOfTrips, numOfTripsWatcher, String.format(Locale.US, "%d", total_trips));
+                amountRemaining.setText(getString(R.string.amount_left_test, remaining_funds));
+                //amountRemaining.setText(getString(R.string.amount_left) + Double.toString(remaining_funds));
                 break;
         }
     }
