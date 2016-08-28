19775c01a05ac3a223ceb5f524a85c0b4f8a23b6
PCGeek <mendelh1537@gmail.com>
2016-08-28 13:06:16 -0400

    added texview with total amount added at end of transaction

--- app/src/main/java/com/pcgeekbrain/subwayfarecalculator/MainActivity.java
+++ app/src/main/java/com/pcgeekbrain/subwayfarecalculator/MainActivity.java
@@ -12,48 +12,49 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText existingFunds, numOfTrips, totalToAdd;
-    private TextView amountRemaining;
+    private EditText existingFundsView, numOfTripsView, totalToAddView;
+    private TextView amountRemainingView, amountOnCardView;
     public static final String TAG = "MainActivity";
     //calculation variables
     private double fare = 2.75;
     private double rate = 1.11;
     private double min = 5.50;
     double remaining_funds = 0;
+    double amountOnCard = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        existingFunds = (EditText)findViewById(R.id.existing_funds);
-        numOfTrips = (EditText)findViewById(R.id.num_of_trips);
-        totalToAdd = (EditText)findViewById(R.id.total_to_add);
-        amountRemaining = (TextView)findViewById(R.id.amount_remaining);
-        amountRemaining.setText(getString(R.string.amount_left_test, remaining_funds));
-
-        existingFunds.addTextChangedListener(ExistingFundsWatcher);
-        numOfTrips.addTextChangedListener(numOfTripsWatcher);
-        totalToAdd.addTextChangedListener(totalToAddWatcher);
+        //get the UI objects
+        existingFundsView = (EditText)findViewById(R.id.existing_funds);
+        numOfTripsView = (EditText)findViewById(R.id.num_of_trips);
+        totalToAddView = (EditText)findViewById(R.id.total_to_add);
+        amountRemainingView = (TextView)findViewById(R.id.amount_remaining);
+        amountOnCardView = (TextView)findViewById(R.id.amount_on_card);
+        //assign the listeners
+        existingFundsView.addTextChangedListener(ExistingFundsWatcher);
+        numOfTripsView.addTextChangedListener(numOfTripsWatcher);
+        totalToAddView.addTextChangedListener(totalToAddWatcher);
     }
 
     private void runCalculation(int id){
         //TODO calculate remaining funds and add to bottom of activity
-        double existing = getDoubleFromEditText(existingFunds);
-        double trips = getDoubleFromEditText(numOfTrips);
-        double total = getDoubleFromEditText(totalToAdd);
+        double existing = getDoubleFromEditText(existingFundsView);
+        double trips = getDoubleFromEditText(numOfTripsView);
+        double total = getDoubleFromEditText(totalToAddView);
         int total_trips;
 
         switch (id){
             case R.id.existing_funds:
             case R.id.num_of_trips:
-                //total to add is num of trips multiplied by the fare. then for good measure remove existing funds
+                //totalToAddView = num of trips multiplied by the fare. then for good measure remove existing funds
                 total = (trips * fare) - existing;
                 if (total > min * rate){    //if we qualify for the discount (are adding more then min (after bonus is applied)
                     total = total / rate;   //divide by rate because x = ( x / y ) * y (division is the reverse of multiplication
                 }
                 //set the total to the amount we need
-                setWatchedEditText(totalToAdd, totalToAddWatcher, String.format(Locale.US, "%.2f", total));
+                setWatchedEditText(totalToAddView, totalToAddWatcher, String.format(Locale.US, "%.2f", total));
                 break;
             case R.id.total_to_add:
                 //add bonus if over min
@@ -62,18 +63,18 @@ public class MainActivity extends AppCompatActivity {
                 }
                 trips = (total + existing) / fare;              //trips available is the amount of times you have the fare on the card
                 total_trips = (int)trips;                       //round that down to avoid rounding up errors (e.g. $5 = 2 trips)
-                remaining_funds = (trips - (int)trips) * fare;  //get whatever is left (remainder of trips times fare. Since trips is the total divided by fare this should work.
 
-                //debug
-                Log.d(TAG, "runCalculation: trips = " + trips);
-                Log.d(TAG, "runCalculation: total_trips = " + total_trips);
-                Log.d(TAG, "runCalculation: remaining_funds = " + remaining_funds);
                 //set text fields
-                setWatchedEditText(numOfTrips, numOfTripsWatcher, String.format(Locale.US, "%d", total_trips));
-                amountRemaining.setText(getString(R.string.amount_left_test, remaining_funds));
-                //amountRemaining.setText(getString(R.string.amount_left) + Double.toString(remaining_funds));
+                setWatchedEditText(numOfTripsView, numOfTripsWatcher, String.format(Locale.US, "%d", total_trips));
                 break;
         }
+        //after fields are set: get amount that will be on card when transaction is done and what will be left
+        remaining_funds = (trips - (int)trips) * fare;  //get whatever is left (remainder of trips times fare. Since trips is the total divided by fare this should work.
+        amountOnCard = trips * fare + remaining_funds;
+
+        amountRemainingView.setText(getString(R.string.amount_left_test, remaining_funds));
+        amountOnCardView.setText(getString(R.string.amount_on_card, amountOnCard));
+
     }
 
     private TextWatcher ExistingFundsWatcher = new TextWatcher() {
